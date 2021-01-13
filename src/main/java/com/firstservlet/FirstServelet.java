8be5a0d71d3072e5e5e7b4bbdc7eda8d52a00913
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-13 10:30:09 +0530

    [Saiprasad]Added: my first servlet

---
+++ src/main/java/com/firstservlet/FirstServelet.java
@@ -0,0 +1,21 @@
+package com.firstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServelet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>Hello world saiprasad first servelet</h3>");
+        out.close();
+    }
+}
